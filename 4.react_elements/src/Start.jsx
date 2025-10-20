72f7ebe0f4d61d9096c7e1021d612fc32d382586
Sai Preetham <saipreetham@Sais-MacBook-Air.local>
2025-10-21 00:27:20 +0530

    Working with attributes like className

---
+++ 4.react_elements/src/Start.jsx
@@ -0,0 +1,13 @@
+import "./Start.css"
+
+let dev = "$@! **7"
+
+function Display(){
+    return (
+        <div className="container">
+            <p className="para">Hello developer : {dev}</p>
+        </div>
+    )
+}
+
+export default Display
