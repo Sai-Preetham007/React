48766d8c380d542229bff7f432b5b08883f42fa9
Sai Preetham <saipreetham@Sais-MacBook-Air.local>
2025-10-21 00:09:09 +0530

    Removed Unwanted Files

---
+++ 3.remove_unwanted_files/src/Start.jsx
@@ -0,0 +1,14 @@
+let dev = "$@! **7"
+
+function Display(){
+    return (
+        <div>
+            <h1>Hi, Welcome</h1>
+            <h3>Look at the clean code with all the un-necessary files are removed.</h3>
+            <br />
+            <p>This is developed by : {dev}</p>
+        </div>
+    )
+}
+
+export default Display
