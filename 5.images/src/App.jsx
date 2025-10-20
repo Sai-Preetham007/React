b7bba829d314bb62e41e8c0db69ae62c14402afc
Sai Preetham <saipreetham@Sais-MacBook-Air.local>
2025-10-21 01:05:50 +0530

    Images

---
+++ 5.images/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className='img_zhao_lusi' src="Images/Zhao Lusi.webp" alt="Zhao Lusi" />
+      <img className='img_anime' src="Images/cool pic.jpg" alt="Anime" />
+    </div>
+  );
+}
+
+export default App;
